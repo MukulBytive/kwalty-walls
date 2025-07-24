32f50abdad79392de4b6ad8a660c5601b7fc9893
devMukulSingh <mukulsingh2276@gmail.com>
2025-07-24 17:33:00 +0530

    feat/ updating images and text based on night and day

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,21 @@ import { useEffect, useState } from "react";
 
 import axios from "axios";
 
-import bgLeft from "./assets/bgLeft.png";
 import topten from "./assets/topten.png";
 import flyingChocoBall from "./assets/flyingChocoball.png";
 import dayBg from "./assets/dayBg.png";
 import buyNowButton from "./assets/buyNowButton.png";
-import tempDay from "./assets/main/tempDay.png";
-import logo from "./assets/main/logo.png";
-import text from "./assets/main/text.png";
-import icecreamBg from "./assets/iceCreameBg.png"
+import tempDay from "./assets/tempDay.png";
+import nightBg from "./assets/nightBg.png";
+import tempNight from "./assets/tempNight.png";
+import logo from "./assets/logo.png";
+import icecreamBg from "./assets/iceCreameBg.png";
 import chocolateBg from "./assets/chocolateBg.png";
 import icecream from "./assets/icecream.png";
 import sixLayersOfExtra from "./assets/6layersOfExtra.png";
 import pack from "./assets/pack.png";
 import textDay from "./assets/textDay.png";
-
-
-
-
-
-
+import textNight from "./assets/textNight.png";
 
 
 // import icecream from "./assets/main/iceCream.png";
@@ -35,14 +30,14 @@ const DEFAULT_LOCATION = {
 function App() {
   const [currTemperature, setCurrTemperature] = useState(30);
   const [loading, setLoading] = useState(false);
+  const [isNight, setIsNight] = useState(false);
 
   useEffect(() => {
     async function getTemperature() {
       setLoading(true);
       try {
         // Make the API request using Axios
-        const location = await getCurrentLocation();
-        console.log({ location });
+        // const location = await getCurrentLocation();
         // const { data } = await axios.get(
         //   "https://api.openweathermap.org/data/2.5/weather",
         //   {
@@ -55,6 +50,11 @@ function App() {
         //   }
         // );
         // setCurrTemperature(data?.main?.temp?.toFixed(0));
+        // const sunriseTime = data.sys.sunrise * 1000; // Convert UNIX timestamp to milliseconds
+        // const sunsetTime = data.sys.sunset * 1000; // Convert UNIX timestamp to milliseconds
+        // const currentTime = new Date().getTime();
+        // const isDaytime = currentTime > sunriseTime && currentTime < sunsetTime;
+        // if(!isDaytime) setIsNight(true)
       } catch (error) {
         console.error("Error fetching temprature data:", error);
         return null;
@@ -72,7 +72,6 @@ function App() {
           async (position) => {
             const lat = position.coords.latitude;
             const long = position.coords.longitude;
-            console.log({ long, lat });
             resolve({ long, lat });
           },
           (error) => {
@@ -93,7 +92,7 @@ function App() {
     });
   }
   return (
-    <div className="font-Sha flex items-center flex-col h-[100vh] justify-center w-full  ">
+    <div className="flex items-center flex-col h-[100vh] justify-center w-full  ">
       <div className="aspect-video bg-neutral-100 h-[30rem]  relative flex rounded-xl">
         {/* FLYING CHOCOBALL BACKGROUND */}
         <img
@@ -101,7 +100,7 @@ function App() {
           className="absolute z-30 self-center  w-full px-5"
         />
         {/* LEFT SECTION */}
-        <section className="h-full w-1/2 flex flex-col relative bg-[url(./assets/bgLeft.png)]">
+        <section className={`h-full w-1/2 flex flex-col relative bg-[url(./assets/bgLeft.png)] `}>
           <img src={icecreamBg} className="w-full h-full absolute" />
           <img src={topten} className="h-20 absolute top-10 right-20" />
           <img src={chocolateBg} className="self-center absolute top-[100px]" />
@@ -117,13 +116,7 @@ function App() {
             src={pack}
             className="absolute w-full  h-[14rem] object-contain bottom-10 right-[-190px] z-40"
           />
-          {/* <div className="bg-[#8b43b0] z-20 w-full h-full absolute opacity-50" /> */}
-          {/* <img src={bgLeft} className="w-full h-full z-10" /> */}
-          {/* <div className="absolute top-0 justify-between pt-8 pb-19 flex flex-col z-30 h-full items-center w-full">
-            <img src={topten} className="h-20" />
-            <img src={text} className="h-40" />
-            <img src={buyNowButton} className="h-12" />
-          </div> */}
+
         </section>
 
         {/* RIGHT SECTION */}
@@ -132,24 +125,26 @@ function App() {
 
           {/* DAY BG */}
           <img
-            src={dayBg}
+            src={isNight ? nightBg : dayBg}
             className="absolute top-0 object-cover z-10 h-full w-full "
           />
           <div className="flex flex-col absolute top-14 z-20 gap-8 w-[70%]">
-
             <div className=" flex z-20 left-0 top-14">
               <div className="relative bg-white rounded-r-full px-2 pr-12 items-center flex">
                 <p className="font-shakila text-3xl text-[#9165c9] font-medium">
                   {currTemperature}&deg;C
                 </p>
-                <img className="h-11 right-0 absolute" src={tempDay} />
+                <img className="h-11 right-0 absolute" src={isNight ? tempNight : tempDay} />
               </div>
             </div>
 
             {/* LOGO */}
             <img src={logo} className="h-14 absolute -top-5 z-20 right-0" />
 
-            <img src={textDay} className="mt-8 ml-6 z-30 w-full object-contain " />
+            <img
+              src={isNight ? textNight : textDay}
+              className="mt-8 ml-6 z-30 w-full object-contain "
+            />
 
             <img
               src={buyNowButton}
